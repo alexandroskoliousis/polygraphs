cbb1295bd3199dc61b5ae6e6b8fc872f1045d320
Alexandros Koliousis <alexandros.koliousis@gmail.com>
2021-05-07 20:58:05 +0100

    Graphs and visualisations

--- polygraphs/visualisations.py
+++ polygraphs/visualisations.py
@@ -1,43 +1,112 @@
+"""
+PolyGraph visualisations
+"""
+import os
+import inspect
+
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, FFMpegWriter, PillowWriter
+
 import networkx as nx
 import dgl
-from enum import Enum
+
+import numpy as np
 
 
-class Layout(Enum):
-
-    CIRCULAR = nx.circular_layout
-
-    PLANAR = nx.planar_layout
-    RANDOM = nx.random_layout
-    SPRING = nx.spring_layout
-    SPIRAL = nx.spiral_layout
-
-
-def _getlayout(graph, layout):
-    if layout:
-        return layout(graph)
-    else:
+def _get_layout(graph, name, **kwargs):
+    if name is None:
         return None
+    assert isinstance(name, str)
+    # Create friendly dictionary from list of (name, function) tuples
+    layouts = dict(inspect.getmembers(nx.drawing.layout, inspect.isfunction))
+    # Find layout routine from name
+    layout = layouts.get(f'{name.lower()}_layout')
+    if layout is None:
+        raise Exception(f'Invalid graph layout: {name}')
+    return layout(graph, **kwargs)
 
 
-def draw(graph, layout=None):
+def draw(graph, figsize=None, layout=None, **kwargs):
     """
-    Draws a graph.
+    Draws a PolyGraph.
     """
-    g = dgl.to_networkx(graph)
-    _, ax = plt.subplots()
-    nx.draw(g,
-            pos=_getlayout(g, layout),
-            node_color='#EBF1DD',
+    # Export beliefs from graph node attributes, if present
+    if 'belief' in graph.ndata:
+        beliefs = graph.ndata.get('belief').numpy()
+    else:
+        print('Beliefs not found')
+        beliefs = np.zeros(graph.num_nodes())
+    print(beliefs)
+    # Convert DGL graph to NetworkX graph
+    G = dgl.to_networkx(graph)  # pylint: disable=invalid-name
+    # Plot
+    fig, ax = plt.subplots(figsize=figsize)  # pylint: disable=invalid-name
+    nx.draw(G,
+            pos=_get_layout(G, layout, **kwargs),
             edgecolors='black',
-            node_size=600,
             with_labels=True,
+            node_color=beliefs,
+            vmin=0.0,
+            vmax=1.0,
+            cmap=plt.cm.coolwarm,  # pylint: disable=no-member
             ax=ax)
+    # Show frame
+    ax.axis('on')
+    # Map scalars to RGBA
+    mapper = plt.cm.ScalarMappable(cmap=plt.cm.coolwarm)  # pylint: disable=no-member
+    # Show colorbar
+    fig.colorbar(mapper)
+    return fig, ax
 
 
-def animate(graph):
+def animate(graph, frames, filename=None, fps=1, figsize=None, layout=None, **kwargs):
     """
     Animate a graph.
     """
-    return None
+    # Sort out destination file
+    if not filename:
+        # Generate a GIF by default in local directory
+        filename = 'animation.gif'
+
+    # Get file extension
+    _, ext = os.path.splitext(filename)
+    if ext not in ['.gif', '.mp4']:
+        raise Exception("Invalid file name: {}".format(filename))
+
+    # Movie writer
+    writer = PillowWriter(fps=fps) if ext == 'gif' else FFMpegWriter(fps=fps)
+
+    # Export beliefs from graph node attributes, if present
+    if 'belief' in graph.ndata:
+        beliefs = graph.ndata.get('belief').numpy()
+    else:
+        beliefs = np.zeros(graph.num_nodes())
+    # Add latest beliefs to the right side of the deque
+    frames.append(beliefs)
+    # Convert DGL graph to NetworkX graph
+    G = dgl.to_networkx(graph)  # pylint: disable=invalid-name
+    # Set plot
+    fig, ax = plt.subplots(figsize=figsize)  # pylint: disable=invalid-name
+    # Map scalars to RGBA
+    mapper = plt.cm.ScalarMappable(cmap=plt.cm.coolwarm)  # pylint: disable=no-member
+    # Show colorbar
+    fig.colorbar(mapper)
+
+    def update(frame):
+        # Redraw graph
+        nx.draw(G,
+                pos=_get_layout(G, layout, **kwargs),
+                edgecolors='black',
+                with_labels=True,
+                node_color=frame,
+                vmin=0.0,
+                vmax=1.0,
+                cmap=plt.cm.coolwarm,  # pylint: disable=no-member
+                ax=ax)
+        ax.axis('on')
+
+    # Create animation
+    animation = FuncAnimation(fig, update, frames=frames, blit=False)
+    # Save animation to file
+    animation.save(filename, writer=writer)
+    return animation
