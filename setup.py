f69871e135dc2244186b16f7a8e9f21dc56f8839
Alexandros Koliousis <alexandros.koliousis@gmail.com>
2021-05-20 11:19:26 +0100

    Project setup (work-in-progress)

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+PolyGraph installation setup
+"""
+
+import setuptools
+
+VERSION = '1'
+
+setuptools.setup(
+    name='polygraphs',
+    version=VERSION,
+    description='PolyGraphs',
+    long_description='',
+    author='Alexandros Koliousis',
+    author_email='ak@akoliousis.com',
+    url='https://github.com/alexandroskoliousis/polygraphs',
+    license='MIT',
+    keywords='test test',
+    packages=setuptools.find_packages(exclude=('tests',)),
+    install_requires=[
+        'torch',
+        'dgl',
+        'notebook',
+        'matplotlib',
+        'PyYaml',
+        'pandas'
+    ],
+    python_requires='>=3',
+)
