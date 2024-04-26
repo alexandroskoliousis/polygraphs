abc6b00affc176c36f5f3c497cf6c933782f60f6
Amil Mohanan <amil@mohanan.net>
2024-04-26 15:28:52 +0100

    Added get_beliefs and get_graphs

--- polygraphs/analysis/__init__.py
+++ polygraphs/analysis/__init__.py
@@ -11,7 +11,7 @@ class Processor(SimulationProcessor, AddAttributes):
     This class inherits from SimulationProcessor and AddAttributes classes,
     allowing it to process simulation data and add attributes to it.
 
-    Parameters:
+    Keyword arguments:
     - root_folder_path (str): The path to the root folder containing simulation data.
     - graph_converter (Graphs, optional): An instance of Graphs class for graph conversion.
         If not provided, a new instance will be created.
@@ -22,6 +22,7 @@ class Processor(SimulationProcessor, AddAttributes):
     instances of Graphs and Beliefs classes. It then processes the simulations
     in the root folder path.
     """
+
     def __init__(self, root_folder_path, graph_converter=None, belief_processor=None):
         # Initialize with default Graphs and Beliefs instances if not provided
         if graph_converter is None:
@@ -40,6 +41,7 @@ class Processor(SimulationProcessor, AddAttributes):
         This method takes a variable number of methods and applies a decorator
         to each method, allowing it to be called to add custom columns to the DataFrame.
         """
+
         def column(func):
             def wrapper(*args, **kwargs):
                 func(*args, **kwargs)
@@ -57,3 +59,49 @@ class Processor(SimulationProcessor, AddAttributes):
         - pandas.DataFrame: The DataFrame containing processed simulation data.
         """
         return self.dataframe
+
+    def get_beliefs(self, row=None):
+        """
+        Get the beliefs for all similation or for a single simualtion
+
+        Keyword arguments:
+        - row: return the beliefs for a specific simulation row from results dataframe
+
+        Returns:
+        - pandas.DataFrame: The DataFrame containing processed simulation data.
+        """
+        if isinstance(row, int) and row >= 0:
+            bin_file_path = self.dataframe.iloc[row]["bin_file_path"]
+            hd5_file_path = self.dataframe.iloc[row]["hd5_file_path"]
+            return self.belief_processor.get_beliefs(
+                hd5_file_path, bin_file_path, self.graph_converter
+            )
+        else:
+            _ = []
+            for hd5_file_path, bin_file_path in zip(
+                self.dataframe["hd5_file_path"], self.dataframe["bin_file_path"]
+            ):
+                iterations = self.belief_processor.get_beliefs(
+                    hd5_file_path, bin_file_path, self.graph_converter
+                )
+                _.append(iterations)
+            return _
+
+    def get_graphs(self, row=None):
+        """
+        Get the graphs for all similation or for a single simualtion
+
+        Keyword arguments:
+        - row: return the graph for a specific simulation row from results dataframe
+
+        Returns:
+        - nx.Graph: A NetworkX Graph of the simulation
+        """
+        if isinstance(row, int) and row >= 0:
+            bin_file_path = self.dataframe.iloc[row]["bin_file_path"]
+            return self.graph_converter.get_networkx_object(bin_file_path)
+        else:
+            return [
+                self.graph_converter.get_networkx_object(bin_file_path)
+                for bin_file_path in self.dataframe["bin_file_path"]
+            ]
